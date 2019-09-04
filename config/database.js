71ef0d30fe8eeb4f5de8695cb7c17fb910141fab
syedabdullah912 <syedabdullah912@gmail.com>
2019-09-04 17:00:16 +0500

    db-linked

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    database:'mongodb://localhost:27017/meanauth',
+    database:'mongodb://syedabdullah912:9dec1993@ds217438.mlab.com:17438/heroku_r4ss1sxv',   
     secret:'yoursecret'
 }
