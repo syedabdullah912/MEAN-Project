07e7cd0d96cf146e7557c84853d0c2ffc7c97ad2
syedabdullah912 <33052382+syedabdullah912@users.noreply.github.com>
2022-03-03 03:53:19 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports={
     //database:'mongodb://localhost:27017/meanauth',                                        // for localhost
     //database: 'mongodb://syedabdullah912:9dec1993@ds217438.mlab.com:17438/heroku_r4ss1sxv', // for heroku
-    database:'mongodb+srv://admin:1234@cluster0.5fhy4.mongodb.net/MEAN-EXP',
-    secret:'yoursecret'
+    //database:'mongodb+srv://admin:1234@cluster0.5fhy4.mongodb.net/MEAN-EXP'
+    database:'mongodb+srv://admin:1234@cluster0.5fhy4.mongodb.net/MEAN-EXP'
+    //secret:'yoursecret'
 }
