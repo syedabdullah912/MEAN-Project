f5dc2d1574aa93af88b7f6995035a6c9ec1366aa
syedabdullah912 <33052382+syedabdullah912@users.noreply.github.com>
2022-03-08 03:08:22 +0500

    Update database.js
    database updated to cloud mongodb atlas.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 module.exports={
-    //database:'mongodb://localhost:27017/meanauth',                                        // for localhost
-    //database: 'mongodb://syedabdullah912:9dec1993@ds217438.mlab.com:17438/heroku_r4ss1sxv', // for heroku
-    //database:'mongodb+srv://admin:1234@cluster0.5fhy4.mongodb.net/MEAN-EXP'
-    database:'mongodb+srv://admin:1234@cluster0.5fhy4.mongodb.net/MEAN-EXP'
-    //secret:'yoursecret'
+    //database:'mongodb://localhost:27017/meanauth',                                                       // for localhost
+    //database: 'mongodb://syedabdullah912:9dec1993@ds217438.mlab.com:17438/heroku_r4ss1sxv',              // for mlab.com
+     database:'mongodb+srv://admin:admin@cluster0.5fhy4.mongodb.net/mean-exp?retryWrites=true&w=majority', // for atlas cloud mongodb
+     secret:'yoursecret'
 }
